2f09220d27a92e60bf81885ba1e9ddabd3eff4b5
ahmedzatar <ahmed.zatar2@gmail.com>
2021-07-12 15:12:02 +0300

    finish lab27

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
-
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
 # Create your tests here.
+
+class SnakesTests(TestCase):
+    def test_list_page_status_code(self):
+        url= reverse('snacks_list')
+        actual=self.client.get(url).status_code
+        expected=200
+        self.assertEqual(actual,expected)
+    def test_list_url_template(self):
+        url= reverse('snacks_list')
+        actual=self.client.get(url)
+        expected='snack_list.html'
+        self.assertTemplateUsed(actual, expected)
+
+    
